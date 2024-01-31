df9ac518cb7c5d9745d1dddcaae9bdf9fdfabf9a
eunseongjo <tkakfkekdk4@gmail.com>
2024-01-31 18:17:56 +0900

    조은성

--- src/main/java/com/ohgiraffers/java/personal/Eunseong/level02/normal01.java
+++ src/main/java/com/ohgiraffers/java/personal/Eunseong/level02/normal01.java
@@ -11,10 +11,17 @@ public class normal01 {
          * 홀수이면 "홀수다.", 홀수가 아니면 "짝수다." 라고 출력하세요.
          * 단, 1~10 사이의 정수가 아닌 경우 "반드시 1~10 사이의 정수를 입력해야 합니다." 를 출력하세요.
          * */
-
-    }
-    public void NestedIfStatement(){
-
+//
+//    }
+//    public void NestedIfStatement(){
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("1 ~ 10 사이의 정수를 하나 입력히세요 : ");
+//
+//        int num = sc.nextInt();
+//
+//        if (num % 2)
 
 
     }
