4e4c9878124571231055d1d5db49cdd3044dc69c
eunseongjo <tkakfkekdk4@gmail.com>
2024-01-31 17:37:05 +0900

    01.31. 문제 테스트

---
+++ src/main/java/com/ohgiraffers/java/personal/Eunseong/level01/basic02.java
@@ -0,0 +1,38 @@
+package com.ohgiraffers.java.personal.Eunseong.level01;
+
+import java.util.Scanner;
+
+public class basic02 {
+
+
+    public static void main(String[] args) {
+
+        /* 정수를 입력 받아 그 수가 짝수인지 홀수인지 판단해서 결과를 출력하세요
+         * 짝수인 경우 "짝수다." 출력, 홀수인 경우 "홀수다." 출력
+         *
+         * -- 입력 예시 --
+         * 정수를 하나 입력하세요 : 5
+         *
+         * -- 출력 예시 --
+         * 홀수다.
+         * */
+
+        basic02 app1 = new basic02();
+        app1.NestedIfStatement();
+
+    }
+    public void NestedIfStatement(){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("정수를 하나 입력하세요 : ");
+
+        int num = sc.nextInt();
+
+        if (num % 2 == 0){
+            System.out.println("짝수다");
+        } else {
+            System.out.println("홀수다");
+        }
+    }
+}
