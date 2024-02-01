754f2f466513af1a190acc0e77ea7c6ef3558e3e
soobin <nunu1101@gmail.com>
2024-02-01 15:50:21 +0900

    commit 윤수빈 - pdf 파일로 보내주신 내용 첨부

---
+++ src/main/java/com/ohgiraffers/java/practice/subin/chap03method/level01/basic/Calculator.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.java.practice.subin.chap03method.level01.basic;
+
+public class Calculator {
+
+    public String checkMethod(String a){
+        return a;
+    }
+
+//    public int sum(int a) {
+//        a =
+//        for ()
+//
+//    }
+    public int checkMaxNumber(int a, int b){
+        return (a > b)? a : b ;
+    }
+
+    public int sumTwoNumber(int a, int b){
+        return (a + b);
+    }
+
+    public int minusTwoNumber(int a, int b) {
+        return (a - b);
+    }
+}
