89ad09b1dd60ccdad065dd6bb0a699c23f46b66c
limchanwool <cksdnf90@gmail.com>
2024-02-02 15:39:15 +0900

    240202 commit ========================================                 CHANGES ======================================== # study 문제풀이 hard 연습문제 변경 # class명 변경   - 찬울
    ********** dotCode ★promise★ **********
    1. 다른사람 Application 건들지 않기
    2. 커밋하기전에 변경사항 확인하기
    3. 프로젝트 오픈시 Pull 우선적으로 받기
    ========================================

--- src/main/java/com/ohgiraffers/java/study/chanWool/Application.java
+++ src/main/java/com/ohgiraffers/java/study/chanWool/Application.java
@@ -33,7 +33,7 @@ public class Application {
             예) 1 2 3 4 5 6 7 8 9 10
          */
 
-        Method normal = new Method();
+        ArrMethod normal = new ArrMethod();
         normal.arrMethod1();
         System.out.println();
         System.out.println("======= Hard =======");
@@ -51,11 +51,10 @@ public class Application {
             예) 1 2 3 4 5 6 7 8 9 10
          */
 
-        Method hard = new Method();
-        int index = hard.inputNum();
-        int iarr2[] = new int[index];
-        int receiveArr[] = hard.inputIndexNum(iarr2);
-        hard.print(receiveArr);
-
+        ArrMethod hard = new ArrMethod();             // 객체생성
+        int index = hard.inputNum();            // 입력받은 index값 선언 후 초기화
+        int iarr2[] = new int[index];           // 입력받은 index값만큼의 배열 생성
+        int resultArr[] = hard.inputIndexNum(iarr2);    // 그 배열에 1부터 입력받은 값을 각 index별 공간에 추가
+        hard.print(resultArr);                  // 최종 배열의 각 index별 공간 값 출력
     }
 }
--- src/main/java/com/ohgiraffers/java/study/chanWool/Quest.java
+++ src/main/java/com/ohgiraffers/java/study/chanWool/Quest.java
@@ -2,7 +2,8 @@ package com.ohgiraffers.java.study.chanWool;
 
 import java.util.Scanner;
 
-public class Method {
+public class Quest {
+
     public void quest1()    {
 
         // 240129 literal 문제만들기
@@ -146,38 +147,4 @@ public class Method {
         }
 
     }
-
-    public void arrMethod1(){
-
-        int iarr[] = new int[10];
-
-        for( int i = 0; i < iarr.length; i++ )   {
-            iarr[i] = i+1;
-            System.out.print(iarr[i] + " ");
-        }
-
-    }
-
-    public int inputNum()  {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("인덱스 길이 값 입력 : ");
-        return sc.nextInt();
-    }
-
-    public int[] inputIndexNum(int[] arrTemp) {
-        int[] arr = arrTemp;
-        for( int i = 0; i < arr.length; i++ ){
-            arr[i] = i+1;
-        }
-        return arr;
-    }
-
-    public void print(int[] arrTemp2) {
-        int[] result = arrTemp2;
-        int index;
-        for (index = 0; index < result.length; index++ ) {
-            System.out.print(result[index] + " ");
-        }
-    }
-
 }
