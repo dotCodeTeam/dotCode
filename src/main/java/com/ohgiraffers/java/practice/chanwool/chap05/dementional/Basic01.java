84c5d4612d8a7d2052c63d407af5fd2d56bf306c
limchanwool <cksdnf90@gmail.com>
2024-02-02 12:15:07 +0900

    240202 commit ========================================                 CHANGES ======================================== chap05 연습문제 추가 - 찬울
    ********** dotCode ★promise★ **********
    1. 다른사람 Application 건들지 않기
    2. 커밋하기전에 변경사항 확인하기
    3. 프로젝트 오픈시 Pull 우선적으로 받기
    ========================================

---
+++ src/main/java/com/ohgiraffers/java/practice/chanwool/chap05/dementional/Basic01.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.java.practice.chanwool.chap05.dementional;
+
+public class Basic01 {
+
+    public static void main(String[] args) {
+
+        /* 행의 길이가 3, 열의 길이가 4인 정수형 2차원 배열을 선언 및 할당하고,
+         * 각 인덱스에 차례대로 1부터 12까지의 정수를 대입한 뒤
+         * 그 값을 출력하는 코드를 작성하시오.
+         *
+         * -- 출력 예시 --
+         * 1  2  3  4
+         * 5  6  7  8
+         * 9 10 11 12
+         *
+         * 단, 출력물의 줄맞춤은 신경쓰지 말고 풀어보세요
+         * */
+
+    }
+}
