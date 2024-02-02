54b0b97b7a7af2d1e7868e8d63f874773f756212
limchanwool <cksdnf90@gmail.com>
2024-02-02 14:13:17 +0900

    240202 commit ========================================                 CHANGES ======================================== 수빈님 chap05 3번 연습문제 변경 - 찬울
    ********** dotCode ★promise★ **********
    1. 다른사람 Application 건들지 않기
    2. 커밋하기전에 변경사항 확인하기
    3. 프로젝트 오픈시 Pull 우선적으로 받기
    ========================================

--- src/main/java/com/ohgiraffers/java/practice/subin/chap05array/normal/Application.java
+++ src/main/java/com/ohgiraffers/java/practice/subin/chap05array/normal/Application.java
@@ -23,11 +23,11 @@ public class Application {
                 exampleNum++;
             }
         }
-            for (int i = 0; i < iarr1.length; i++) {
-                for (int j = 0; j < iarr1[i].length; j++) {
-                    System.out.print(iarr1[i][j] + "\t");
-                }
-                System.out.println();
+        for (int i = 0; i < iarr1.length; i++) {
+            for (int j = 0; j < iarr1[i].length; j++) {
+                System.out.print(iarr1[i][j] + "\t");
+            }
+            System.out.println();
         }
     }
 }
