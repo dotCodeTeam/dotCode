39dbfd9a5376296b028fc23c8298becc961dfc05
cwul94 <cksdnf90@gmail.com>
2024-02-03 00:24:35 +0900

    240202 commit =======================================                 CHANGES ======================================= # study 배열 연습문제 코드 수정 # study.chanWool
                        Changed by chanwool
    
    ********   ★Github promise★   ********
    1. 다른사람 Application 건들지 않기
    2. 커밋하기전에 변경사항 확인하기
    3. 프로젝트 오픈시 Pull 우선적으로 받기
    =======================================

--- src/main/java/com/ohgiraffers/java/study/chanWool/Application.java
+++ src/main/java/com/ohgiraffers/java/study/chanWool/Application.java
@@ -11,7 +11,7 @@ public class Application {
          */
         System.out.println("======= Basic ========");
         System.out.print(" ");
-        int arr[] = new int[10];
+        int[] arr = new int[10];
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 1;
