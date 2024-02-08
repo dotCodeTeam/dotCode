f8f02834c49def3bd917b009304bcf8177a6afe2
limchanwool <cksdnf90@gmail.com>
2024-02-08 12:29:58 +0900

    240208 Commit
    # 다형성 디렉토리 추가

---
+++ src/main/java/com/ohgiraffers/java/practice/chanwool/polymorphism/dotCode/Chanwool.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.java.practice.chanwool.polymorphism.dotCode;
+
+public class Chanwool extends Developer {
+
+
+
+}
---
+++ src/main/java/com/ohgiraffers/java/practice/chanwool/polymorphism/dotCode/Human.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.java.practice.chanwool.polymorphism.dotCode;
+
+public class Human {
+
+    public void walk(){
+        System.out.println("걸어갑니다.");
+    }
+    public void run(){
+        System.out.println("뛰어갑니다.");
+    }
+    public void talk(){
+        System.out.println("말합니다.");
+    }
+    public void eat(){
+        System.out.println("밥을 먹습니다.");
+    }
+    public void sleep(){
+        System.out.println("잠을 잡니다.");
+    }
+}
